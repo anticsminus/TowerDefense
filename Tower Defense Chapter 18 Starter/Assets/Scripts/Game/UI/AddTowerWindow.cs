acbc11d865de1a634be3e060216c03c53865c586
SLCTECH\clandry <antics_chris@hotmail.com>
2018-11-22 10:11:49 -0500

    [Scripts] Added UI scripts to purchase towers Added functionality for stone tower

---
+++ Tower Defense Chapter 18 Starter/Assets/Scripts/Game/UI/AddTowerWindow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AddTowerWindow : MonoBehaviour {
+    //1
+    public GameObject towerSlotToAddTowerTo;
+    //2
+    public void AddTower(string towerTypeAsString)
+    {
+        //3
+        TowerType type = (TowerType)Enum.Parse(typeof(TowerType),
+        towerTypeAsString, true);
+        //4
+        if (TowerManager.Instance.GetTowerPrice(type) <=
+        GameManager.Instance.gold)
+        {
+            //5
+            GameManager.Instance.gold -= TowerManager.Instance
+            .GetTowerPrice(type);
+            //6
+            TowerManager.Instance.CreateNewTower(towerSlotToAddTowerTo, type);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Tower Defense Chapter 18 Starter/Assets/Scripts/Tower/TowerManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+[Serializable]
+public struct TowerCost
+{
+    public TowerType TowerType;
+    public int Cost;
+}
+public class TowerManager : MonoBehaviour
+{
+
+    //1
+    public static TowerManager Instance;
+    //2
+    public GameObject stoneTowerPrefab;
+    public GameObject fireTowerPrefab;
+    public GameObject iceTowerPrefab;
+    //3
+    public List<TowerCost> TowerCosts = new List<TowerCost>();
+    //4
+    void Awake()
+    {
+        Instance = this;
+    }
+    //5
+    public void CreateNewTower(GameObject slotToFill, TowerType towerType)
+    {
+        switch (towerType)
+        {
+            case TowerType.Stone:
+                Instantiate(stoneTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+            case TowerType.Fire:
+                Instantiate(fireTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+            case TowerType.Ice:
+                Instantiate(iceTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+        }
+    }
+    //6
+    public int GetTowerPrice(TowerType towerType)
+    {
+        return (from towerCost in TowerCosts where towerCost.TowerType == towerType select towerCost.Cost).FirstOrDefault();
+    }
+}
